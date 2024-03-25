041522c163bd148ceffe2948d5b0e1985c264405
Lieven Tytgat <lieven.tytgat@tymeli.be>
2024-03-25 15:28:57 +0100

    Fix USB REPL
    For REPL not over uart-usb convertor

--- ports/esp32/boards/ESP32_GENERIC_C6/mpconfigboard.cmake
+++ ports/esp32/boards/ESP32_GENERIC_C6/mpconfigboard.cmake
@@ -2,7 +2,9 @@ set(IDF_TARGET esp32c6)
 
 set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
+    ${SDKCONFIG_IDF_VERSION_SPECIFIC}
     #boards/sdkconfig.ble
+    boards/sdkconfig.usb
     boards/ESP32_GENERIC_C6/sdkconfig.board
 )
 
